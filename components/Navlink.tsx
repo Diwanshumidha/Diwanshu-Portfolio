b4d96dce5e2e9b4bd446e34a5020993fbb861b3c
Diwanshu <saidiwanshu1880@gmail.com>
2023-10-31 23:32:55 +0530

    Fixed Unused Imports

--- components/Navlink.tsx
+++ components/Navlink.tsx
@@ -1,26 +1,24 @@
-
-import React, { Children } from "react";
+import React from "react";
 import { usePathname } from "next/navigation";
-import {cn} from "@/lib/utils";
+import { cn } from "@/lib/utils";
 import Link, { LinkProps } from "next/link";
 
 type NavLinkProps = React.PropsWithChildren<LinkProps> & {
-  activeClassName?: string,
-  className?:string;
+  activeClassName?: string;
+  className?: string;
 };
 
 export const NavLink = ({
   children,
-  className:classn,
+  className: classn,
   activeClassName = "active",
   ...props
 }: NavLinkProps) => {
-
   const path = usePathname();
 
   const isActive = path === props.href || path === props.as;
 
-  const className = cn(classn, {[activeClassName]: isActive });
+  const className = cn(classn, { [activeClassName]: isActive });
 
   return (
     <Link {...props} className={className}>
