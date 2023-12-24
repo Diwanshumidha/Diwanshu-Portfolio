9bdf2724cefab5b05ca6e4f460bbd1d3878752ea
Diwanshu <saidiwanshu1880@gmail.com>
2023-12-24 20:32:09 +0530

    Fix: Scroll Component

--- components/Home/Projects/Projects.tsx
+++ components/Home/Projects/Projects.tsx
@@ -21,7 +21,7 @@ const Projects = async () => {
 
   return (
     <section className="flex flex-col text-left justify-between pt-8 relative my-14">
-      <div id="learnmore">
+      <div id="learnmore" className=" scroll-m-24">
         <div className="overflow-x-hidden w-full pt-5">
           <h2 className="landingSectionTitle max-w-sm md:max-w-max mx-auto md:mx-0 text-center md:text-left relative text-3xl font-bold mb-10 pt-4 md:pt-0 md:w-max">
             Here are a few of my favorite projects.
